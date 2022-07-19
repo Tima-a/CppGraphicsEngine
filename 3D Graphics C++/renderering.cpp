6ace78d90c4a2ca13b8f56945e80ff28e092d99a
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2022-07-19 14:40:32 +0400

    Update renderering.cpp

--- 3D Graphics C++/renderering.cpp
+++ 3D Graphics C++/renderering.cpp
@@ -345,7 +345,10 @@ namespace vector2d
 									break;
 								}
 								x_modifier += x_increasement;
+								if (x == floor(fabs(x_addition)))
+								{
 									draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
+								}
 							}
 						}
 						else if (x_y_outweight == true)
@@ -391,7 +394,10 @@ namespace vector2d
 									break;
 								}
 								y_modifier += y_increasement;
+								if (y == floor(fabs(x_addition)))
+								{
 									draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
+								}
 							}
 						}
 						else if (x_y_outweight == false)
@@ -990,16 +996,16 @@ namespace vector2d
 			float adx = fabs(x2 - x1);
 			float ady = fabs(y2 - y1);
 			float x_modifier = 0.0f, y_modifier = 0.0f;
-			//for (int i = 0; i < 5; i++)
-			//{
-			//	draw_pixel(x1, y1 + i, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
-			//	draw_pixel(x2, y2 + i, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
-			//}
-			//draw_pixel(0.0f, 0.0f, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
+			for (int i = 0; i < 5; i++)
+			{
+				draw_pixel(x1, y1 + i, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
+				draw_pixel(x2, y2 + i, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
+			}
+			draw_pixel(0.0f, 0.0f, PIXEL_SIZE, PIXEL_SIZE, rgb(255, 255, 0));
 			int px_i = 0;
+			float x_addition = 1.0f, y_addition = 1.0f;
 			if (straight_line_drawing == false && draw_just_pixel == false)
 			{
-				float x_addition = 1.0f, y_addition = 1.0f;
 				int max_pixels = 0;
 				fabs(x1) > fabs(x2) && fabs(y1) > fabs(y2) ? xy_swap = true : xy_swap = false;
 				if (xy_swap == true)
@@ -1098,19 +1104,22 @@ namespace vector2d
 									break;
 								}
 								x_modifier += x_increasement;
-								draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
-								if (save_pixels_matrix == true)
+								if (x == floor(fabs(x_addition)))
 								{
-									matrix_pixels[g].x = x1 + x_modifier;
-									matrix_pixels[g].y = y1 + y_modifier;
+									draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
+									if (save_pixels_matrix == true)
+									{
+										matrix_pixels[g].x = x1 + x_modifier;
+										matrix_pixels[g].y = y1 + y_modifier;
+									}
+									if (save_pixels_matrix_x_cycle == true)
+									{
+										matrix_pixels_x_cycle[p].x = x1 + x_modifier;
+										matrix_pixels_x_cycle[p].y = y1 + y_modifier;
+									}
+									g += 1;
+									p += 1;
 								}
-								if (save_pixels_matrix_x_cycle == true)
-								{
-									matrix_pixels_x_cycle[p].x = x1 + x_modifier;
-									matrix_pixels_x_cycle[p].y = y1 + y_modifier;
-								}
-								g += 1;
-								p += 1;
 							}
 						}
 						else if (x_y_outweight == true)
@@ -1167,20 +1176,27 @@ namespace vector2d
 									break_loop = true;
 									break;
 								}
+								if (x1 == 15.0f)
+								{
+									int h = 0;
+								}
 								y_modifier += y_increasement;
-								draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
-								if (save_pixels_matrix == true)
+								if (y == floor(fabs(cl_y_add))-1)
 								{
-									matrix_pixels[g].x = x1 + x_modifier;
-									matrix_pixels[g].y = y1 + y_modifier;
+									draw_pixel(x1 + x_modifier, y1 + y_modifier, PIXEL_SIZE, PIXEL_SIZE, color);
+									if (save_pixels_matrix == true)
+									{
+										matrix_pixels[g].x = x1 + x_modifier;
+										matrix_pixels[g].y = y1 + y_modifier;
+									}
+									if (save_pixels_matrix_y_cycle == true)
+									{
+										matrix_pixels_y_cycle[q].x = x1 + x_modifier;
+										matrix_pixels_y_cycle[q].y = y1 + y_modifier;
+									}
+									g += 1;
+									q += 1;
 								}
-								if (save_pixels_matrix_y_cycle == true)
-								{
-									matrix_pixels_y_cycle[q].x = x1 + x_modifier;
-									matrix_pixels_y_cycle[q].y = y1 + y_modifier;
-								}
-								g += 1;
-								q += 1;
 							}
 						}
 						else if (x_y_outweight == false)
@@ -1407,7 +1423,7 @@ namespace vector2d
 }
 static void draw_coordinate_grid(unsigned int color)
 {
-	vector2d::VECTOR v1(-100.0f, 0.0f, 100.0f, 0.0f, color, true);
+	vector2d::VECTOR v1(-150.0f, 0.0f, 150.0f, 0.0f, color, true);
 	vector2d::VECTOR v2(0.0f, -50.0f, 0.0f, 50.0f, color, true);
 }
 namespace cube
@@ -1504,24 +1520,11 @@ namespace triangle2d
 		void draw_triangle(float a, float b, float c, float h_apex, float x, float y, unsigned int color, bool filled)
 		{
 			vector2d::VECTOR_SAVE_PIXELS_POSITIONS v1(x - c / 2.0f, y, x + c / 2.0f, y, color, true, 1, 1, true, true, true); //c-line
-			vector2d::VECTOR_SAVE_PIXELS_POSITIONS v2(x - c / 2.0f, y, (x - c / 2.0f) + c-b, y + h_apex, color, true, 1, 1, true, true, true); //a-line
-			vector2d::VECTOR_SAVE_PIXELS_POSITIONS v3(x + c / 2.0f, y, (x - c / 2.0f) + c - b, y + h_apex, color, true, 1, 1, true, true, true); //b-line
+			vector2d::VECTOR_SAVE_PIXELS_POSITIONS v2(x - c / 2.0f, y, (x - c / 2.0f) + a, y + h_apex, color, true, 1, 1, true, true, true); //a-line
+			vector2d::VECTOR_SAVE_PIXELS_POSITIONS v3(x + c/2.0f, y, v2.x2, v2.y2, color, true, 1, 1, true, true, true); //b-line
 			//Drawing triangle works simply by drawing the base line(c-line), then drawing a- or b-line depending on which is bigger and connecting them to each other.
 			//Filling triangle algorithm works simply by connecting all c-line pixels x, y with a-line only y_cycle pixels x, y.
-			float v1y = v1.matrix_pixels[0].y;
-			//if (a > b)
-			//{
-			//	v2.startx = x - c / 2.0f; v2.starty = y; v2.x2 = (x - c / 2.0f) + a; v2.y2 = y + h_apex; color; v2.draw_();
-			//	v3.startx = x + c / 2.0f; v3.starty = y; v3.x2 = v2.x2; v3.y2 = v2.y2; v3.draw_();
-			//}
-			//if (a < b)
-			//{
-			//	float a_ = c - b;
-			//	v2.startx = x - c / 2.0f; v2.starty = y; v2.x2 = (x - c / 2.0f) + a_; v2.y2 = y + h_apex; color; v2.draw_();
-			//	v3.startx = x + c / 2.0f; v3.starty = y; v3.x2 = v2.x2; v3.y2 = v2.y2; v3.draw_();
-			//	//v3.startx = x + c / 2.0f; v3.starty = y; v3.x2 = (x + c / 2.0f) - b; v3.y2 = y + h_apex; color; v3.draw_();
-			//	//v2.startx = x - c / 2.0f; v2.starty = y; v2.x2 = v3.x2; v2.y2 = v3.y2; v2.draw_();
-			//}
+			float v1y = v1.matrix_pixels[0].y; 
 			if (filled == true)
 			{
 				if (a > b)
@@ -1532,26 +1535,11 @@ namespace triangle2d
 						vector2d::VECTOR v4(mxp_v1, v1y, mxp_v1, v2.matrix_pixels_y_cycle[j].y, color, true);
 						//y1 is never changing because c is straight horizontal vector and there is no need to get it every time if it does not alter. Only y2 is a changing value of every a-line pixel's y.
 					}
-					for (int z = (a * 10.0f) - 1; z < (a * 10.0f) + v3.px_quantity / 2; z++)
+					for (int z = (a * 10.0f)-1; z < (a * 10.0f) + v3.px_quantity/2; z++)
 					{
-						int r = int(v3.px_quantity / 2 - (z - ((a * 10.0f) - 1)));
+						int r = int(v3.px_quantity/2 - (z - ((a * 10.0f) - 1)));
 						float mxp_v1 = v1.matrix_pixels[z].x; //mxp_v1 is the pixel's x of c- and a-line and they must be equal to build straight upward vector. Thus, here program gets one variable to not get it twice in x1 and x2
 						vector2d::VECTOR v4(mxp_v1, v1y, mxp_v1, v3.matrix_pixels_y_cycle[r].y, color, true);
-						//y1 is never changing because c is straight horizontal vector and there is no need to get it every time if it does not alter. Only y2 is a changing value of every a-line pixel's y.
-					}
-				}
-				if (a < b)
-				{
-					for (int j = b * 10.0f; j > 0; j--)
-					{
-						float mxp_v1 = v1.matrix_pixels[int(v1.px_quantity - j)].x; //mxp_v1 is the pixel's x of c- and a-line and they must be equal to build straight upward vector. Thus, here program gets one variable to not get it twice in x1 and x2
-						vector2d::VECTOR v4(mxp_v1, v1y, mxp_v1, v3.matrix_pixels_y_cycle[j].y, color, true);
-						//y1 is never changing because c is straight horizontal vector and there is no need to get it every time if it does not alter. Only y2 is a changing value of every a-line pixel's y.
-					}
-					for (int z = v1.px_quantity-b*10.0f; z > 0; z--)
-					{
-						float mxp_v1 = v1.matrix_pixels[z].x; //mxp_v1 is the pixel's x of c- and a-line and they must be equal to build straight upward vector. Thus, here program gets one variable to not get it twice in x1 and x2
-						vector2d::VECTOR v4(mxp_v1, v1y, mxp_v1, v2.matrix_pixels_y_cycle[z].y, color, true);
 						//y1 is never changing because c is straight horizontal vector and there is no need to get it every time if it does not alter. Only y2 is a changing value of every a-line pixel's y.
 					}
 				}
