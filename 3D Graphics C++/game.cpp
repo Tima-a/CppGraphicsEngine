15a57e5f35e50d5359d19ffb54e93a1c8dbda874
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2022-07-19 14:39:35 +0400

    Update game.cpp

--- 3D Graphics C++/game.cpp
+++ 3D Graphics C++/game.cpp
@@ -1,14 +1,23 @@
-int h = 0;
 static void simulategame()
 {
 	//ellipse2d::ELLIPSE e1(0.0f, 0.0f, 30.0f, 20.0f, blue, 3600, true, true, false, false, true);
-	//triangle2d::TRIANGLE t1(6.0f, 4.0f, 10.0f, 5.0f, 0.0f, 0.0f, blue, true);
-	vector2d::VECTOR v1(0.0f, 0.0f, 10.0f, 0.0f, blue, true, 1, 1, true, true, true, true); v1.draw();
-	//draw_coordinate_grid(white);
+	//for (int p = 0; p < 10; p++)
+	//{
+	triangle2d::TRIANGLE t1(16.0f, 13.0f, 20.0f, 12.0f, 0.0f, 0.0f, white, true);
+	//}
+//	vector2d::VECTOR_SAVE_PIXELS_POSITIONS v3(20.0f, 0.0f, -6.0f, 12.0f, red, true, 1, 1, true, true, true); //b-line
+	//vector2d::VECTOR v4(-3.7f, 0.0f, -3.8f, 4.1f, white, true);
+	//vector2d::VECTOR_SAVE_PIXELS_POSITIONS v1(0.0f, 0.0f, 10.0f, 10.0f, white, true, 1, 1, true, true, true);
+	draw_coordinate_grid(white);
+	//for (int z = 0; z < 10000; z++)
+	//{
+	//}
+	//vector2d::VECTOR v1(0.0f, 0.0f, 0.0f, 1.0f, red, true);
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	draw_pixel(0.0f+i * 0.1f, 0.0f + i * 0.1f, PIXEL_SIZE, PIXEL_SIZE, white);
+	//}
 	//float n = make_float_divisible(10.0f, 3.0f);
 	//HWND hWnd = FindWindow("Game Window Class", "Tic Tac Toe");
 	//MessageBox(hWnd, "Error because of vector", "Error", 0x00000000L);
-
-	h += 0.1f;
 }
-
