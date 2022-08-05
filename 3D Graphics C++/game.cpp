3cab9cb76a9000d50c7779c74f97cc8455dd01aa
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2022-08-06 01:30:44 +0400

    Update game.cpp

--- 3D Graphics C++/game.cpp
+++ 3D Graphics C++/game.cpp
@@ -1,11 +1,12 @@
 static void simulategame()
 {
-    //Texture t1("sky.png", 10.0f, 20.0f);
-    //t1.show_image();
-    text2d::text txt("Maybe, I will solve that 2+2!=5?MOMO",0.0f, 0.0f, 2.0f, 2.0f, 1.0f, red, true);
+    //Texture t1("sky.png");
+    //Sprite s1(t1, 0.0f, -1.0f, 1.9f, 1.5f, true);
+    const char* a = ToString(914);
+    text2d::text txt(a,0.0f, 0.0f, 2.0f, 2.0f, 1.0f, red, true);
     //ellipse2d::ELLIPSE e1(0.0f, 0.0f, 10.0f, 10.0f, yellow, 0, 3600, true, true);
     //vector2d::fvector v1(0.0f, 0.0f,40.0f, 40.0f, red, true);
-    //draw_coordinate_grid(white);
+    draw_coordinate_grid(white);
     //HWND hWnd = FindWindow("Game Window Class", "Tic Tac Toe");
     //MessageBox(hWnd, "Error because of vector", "Error", 0x00000000L);
     update_screen(black);
