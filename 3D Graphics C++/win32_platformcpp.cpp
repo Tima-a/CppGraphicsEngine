9597e2d1ac2b38c4094662cfe4ba529a800120f4
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2022-09-05 16:00:50 +0400

    Added Dynamic Run-time Window Creating Algorithm

--- 3D Graphics C++/win32_platformcpp.cpp
+++ 3D Graphics C++/win32_platformcpp.cpp
@@ -1,43 +1,92 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+//Multiple Windows
 #include <windows.h>
 #include "utility.cpp"
 #include <cmath>
+#define MAX_WINDOW_QUANTITY 100
 #define delta_time_equalization 1000000
 static bool running = true;
-struct Screen
-{
-	bool update_screen = false;
-	uint32 scr_refresh_color = 0;
-};
-Screen screen;
-struct Render
-{
-	void* memory;
-	int width, height;
-	float x_pixel_size;
-	float y_pixel_size;
-	float render_scale_height;
-	float screen_half_width;
-	float screen_half_height;
-	BITMAPINFO bitmap_info;
-};
-Render render;
-LPCTSTR window_name = "C++ Graphics";
+int win_count = 0;
+//HWND hWnd = FindWindow("Game Window Class", window_name);
 
-HWND hWnd = FindWindow("Game Window Class", window_name);
 static struct gTime
 {
 	float delta_time;
 };
 gTime gtime;
+struct _Win
+{
+	void* win_memory[MAX_WINDOW_QUANTITY];
+	int win_width[MAX_WINDOW_QUANTITY];
+	int win_height[MAX_WINDOW_QUANTITY];
+	BITMAPINFO bitmap_info[MAX_WINDOW_QUANTITY];
+	LPCSTR window_name[MAX_WINDOW_QUANTITY];
+	HWND windows[MAX_WINDOW_QUANTITY];
+	HDC hdcs[MAX_WINDOW_QUANTITY];
+	HINSTANCE hInstance_;
+};
+_Win _win;
+int size[MAX_WINDOW_QUANTITY];
+static class Window
+{
+public:
+	float x_pixel_size;
+	float y_pixel_size;
+	float render_scale_height;
+	float screen_half_width;
+	float screen_half_height;
+	Window()
+	{
+		//win_count++;
+	}
+	Window(int width, int height, LPCSTR win_name)
+	{
+		_win.win_width[win_count] = width;
+		_win.win_height[win_count] = height;
+		_win.window_name[win_count] = win_name;
+		win_count++;
+		InitializeWinBitmapMemory();
+		InitializeWindow();
+	}
+	void InitializeWinBitmapMemory()
+	{
+		int size[MAX_WINDOW_QUANTITY];
+		for (int i = 0; i < win_count; i++)
+		{
+			size[i] = _win.win_width[i] * _win.win_height[i] * sizeof(unsigned int);
+
+			if (_win.win_memory[i])
+			{
+				VirtualFree(_win.win_memory[i], 0, MEM_RELEASE);
+			}
+			_win.win_memory[i] = VirtualAlloc(0, size[i], MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			_win.bitmap_info[i].bmiHeader.biSize = sizeof(_win.bitmap_info[i].bmiHeader);
+			_win.bitmap_info[i].bmiHeader.biWidth = _win.win_width[i];
+			_win.bitmap_info[i].bmiHeader.biHeight = _win.win_height[i];
+			_win.bitmap_info[i].bmiHeader.biPlanes = 1;
+			_win.bitmap_info[i].bmiHeader.biBitCount = 32;
+			_win.bitmap_info[i].bmiHeader.biCompression = BI_RGB;
+		}
+	}
+	void InitializeWindow()
+	{
+		/*LPCSTR lpszClassName = "Game Wnidow Class";
+		for (int i = 0; i < win_count; i++)
+		{
+			_win.windows[i] = CreateWindow(lpszClassName, _win.window_name[i], WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0,0, _win.win_width[i], _win.win_height[i], 0, 0, _win.hInstance_[i], 0);
+			_win.hdcs[i] = GetDC(_win.windows[i]);
+		}*/
+	}
+};
+
 //#include <vector>
 //template <class T>
 //std::vector<T> name;
 #include "rendering.cpp"
 #include <time.h>
 #include "game.cpp"
-typedef unsigned int uint32;
+
 LRESULT CALLBACK Window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	//This is function which sends a message when something important is happened(input, minimize, maximize, size changed
@@ -52,27 +101,23 @@ LRESULT CALLBACK Window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	break;
 	case WM_SIZE:
 	{
-		RECT rect;
-		GetClientRect(hwnd, &rect);
-		render.width = rect.right - rect.left;
-		render.height = rect.bottom - rect.top;
-		render.x_pixel_size = PIXEL_SIZE_DEF * render.height * render_scale;
-		render.y_pixel_size = PIXEL_SIZE_DEF * render_scale * render_scale;
-		render.render_scale_height = render.height * render_scale;
-		render.screen_half_width = (float)render.width / 2.0f;
-		render.screen_half_height = (float)render.height / 2.0f;
-		int size = render.width * render.height * sizeof(uint32);
-		if (render.memory)
+		int size[MAX_WINDOW_QUANTITY];
+		for (int i = 0; i < win_count; i++)
 		{
-			VirtualFree(render.memory, 0, MEM_RELEASE);
+			size[i] = _win.win_width[i] * _win.win_height[i] * sizeof(unsigned int);
+
+			if (_win.win_memory[i])
+			{
+				VirtualFree(_win.win_memory[i], 0, MEM_RELEASE);
+			}
+			_win.win_memory[i] = VirtualAlloc(0, size[i], MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+			_win.bitmap_info[i].bmiHeader.biSize = sizeof(_win.bitmap_info[i].bmiHeader);
+			_win.bitmap_info[i].bmiHeader.biWidth = _win.win_width[i];
+			_win.bitmap_info[i].bmiHeader.biHeight = _win.win_height[i];
+			_win.bitmap_info[i].bmiHeader.biPlanes = 1;
+			_win.bitmap_info[i].bmiHeader.biBitCount = 32;
+			_win.bitmap_info[i].bmiHeader.biCompression = BI_RGB;
 		}
-		render.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-		render.bitmap_info.bmiHeader.biSize = sizeof(render.bitmap_info.bmiHeader);
-		render.bitmap_info.bmiHeader.biWidth = render.width;
-		render.bitmap_info.bmiHeader.biHeight = render.height;
-		render.bitmap_info.bmiHeader.biPlanes = 1;
-		render.bitmap_info.bmiHeader.biBitCount = 32;
-		render.bitmap_info.bmiHeader.biCompression = BI_RGB;
 	}
 	default:
 	{
@@ -91,9 +136,11 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	//Register Class
 	RegisterClass(&window_class);
-	HWND window = CreateWindow(window_class.lpszClassName, window_name, WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1000, 600, 0, 0, hInstance, 0);
-	HDC hdc = GetDC(window);
-
+	for (int i = 0; i < win_count; i++)
+	{
+		_win.windows[i] = CreateWindow(window_class.lpszClassName, _win.window_name[i], WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, _win.win_width[i], _win.win_height[i], 0, 0, hInstance, 0);
+		_win.hdcs[i] = GetDC(_win.windows[i]);
+	}
 	float delta_time = 0.016666f;
 	LARGE_INTEGER frame_begin_time;
 	QueryPerformanceCounter(&frame_begin_time);
@@ -108,18 +155,22 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	while (running)
 	{
 		//Input
-		MSG message;
-		while (PeekMessage(&message, window, 0, 0, PM_REMOVE))
+		MSG messages[MAX_WINDOW_QUANTITY];
+		for (int j = 0; j < win_count; j++)
 		{
-			TranslateMessage(&message);
-			DispatchMessage(&message);
+			while (PeekMessage(&messages[j], _win.windows[j], 0, 0, PM_REMOVE))
+			{
+				TranslateMessage(&messages[j]);
+				DispatchMessage(&messages[j]);
+			}
 		}
 		//Simulate
-		refresh_screen(screen.scr_refresh_color);
 		simulategame();
 		//Creating Window
-		StretchDIBits(hdc, 0, 0, render.width, render.height, 0, 0, render.width, render.height, render.memory, &render.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
-
+		for (int z = 0; z < win_count; z++)
+		{
+			StretchDIBits(_win.hdcs[z], 0, 0, _win.win_width[z], _win.win_height[z], 0, 0, _win.win_width[z], _win.win_height[z], _win.win_memory[z], &_win.bitmap_info[z], DIB_RGB_COLORS, SRCCOPY);
+		}
 		LARGE_INTEGER frame_end_time;
 		QueryPerformanceCounter(&frame_end_time);
 		gtime.delta_time = ((float)(frame_end_time.QuadPart - frame_begin_time.QuadPart) / perfomance_frequency) * delta_time_equalization; //delta time in 1s
