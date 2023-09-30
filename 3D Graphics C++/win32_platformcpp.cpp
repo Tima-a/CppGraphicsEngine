b15d984c6581f4c38f65d2c8286c5954c87a9911
Teymur Aghayev <107584760+Tima-a@users.noreply.github.com>
2023-09-30 19:29:41 +0400

    Update win32_platformcpp.cpp

--- 3D Graphics C++/win32_platformcpp.cpp
+++ 3D Graphics C++/win32_platformcpp.cpp
@@ -1,53 +1,76 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-//Multiple Windows
+#include <limits.h>
 #include <windows.h>
 #include "utility.cpp"
 #include <cmath>
 #define MAX_WINDOW_QUANTITY 100
 #define delta_time_equalization 1000000
-static bool running = true;
 int win_count = 0;
-//HWND hWnd = FindWindow("Game Window Class", window_name);
-
+static bool all_win_active = true;
+const float window_pixel_coefficent = 2.5f; // Proportion of window's size in microsoft pixels with my pixels.
+const float fixed_width = 0.0f;
+const float fixed_height = 40.0f;
 static struct gTime
 {
-	float delta_time;
+	float delta_time = 0.01f;
 };
 gTime gtime;
+struct Screen
+{
+	bool update_screen_bool[MAX_WINDOW_QUANTITY] = { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true };;
+	uint32 scr_refresh_color[MAX_WINDOW_QUANTITY] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+};
+Screen screen;
 struct _Win
 {
 	void* win_memory[MAX_WINDOW_QUANTITY];
 	int win_width[MAX_WINDOW_QUANTITY];
 	int win_height[MAX_WINDOW_QUANTITY];
+	int win_width_px[MAX_WINDOW_QUANTITY];
+	int win_height_px[MAX_WINDOW_QUANTITY];
+	int winx[MAX_WINDOW_QUANTITY];
+	int winy[MAX_WINDOW_QUANTITY];
 	BITMAPINFO bitmap_info[MAX_WINDOW_QUANTITY];
 	LPCSTR window_name[MAX_WINDOW_QUANTITY];
 	HWND windows[MAX_WINDOW_QUANTITY];
 	HDC hdcs[MAX_WINDOW_QUANTITY];
 	HINSTANCE hInstance_;
+	bool win_can_be_closed[MAX_WINDOW_QUANTITY] = { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true };
 };
 _Win _win;
+static bool visible_windows[MAX_WINDOW_QUANTITY] = { true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true };
 int size[MAX_WINDOW_QUANTITY];
-static class Window
+static class WINDOW
 {
 public:
-	float x_pixel_size;
-	float y_pixel_size;
-	float render_scale_height;
-	float screen_half_width;
-	float screen_half_height;
-	Window()
+	int wnd_param = 0;
+	int x = 0;
+	int y = 0;
+	int width = 0;
+	int height = 0;
+	LPCSTR name = "";
+	WINDOW()
 	{
 		//win_count++;
 	}
-	Window(int width, int height, LPCSTR win_name)
+	WINDOW(LPCSTR win_name, int x_, int y_, int width_, int height_)
 	{
-		_win.win_width[win_count] = width;
-		_win.win_height[win_count] = height;
+		x = x_;
+		y = y_;
+		width = width_;
+		height = height_;
+		name = win_name;
+		_win.win_width[win_count] = width_;
+		_win.win_height[win_count] = height_;
+		_win.win_width_px[win_count] = width_ - 15;
+		_win.win_height_px[win_count] = height_ - 41;
 		_win.window_name[win_count] = win_name;
+		_win.winx[win_count] = x_;
+		_win.winy[win_count] = y_;
+		wnd_param = win_count;
 		win_count++;
 		InitializeWinBitmapMemory();
-		InitializeWindow();
 	}
 	void InitializeWinBitmapMemory()
 	{
@@ -69,14 +92,57 @@ public:
 			_win.bitmap_info[i].bmiHeader.biCompression = BI_RGB;
 		}
 	}
-	void InitializeWindow()
+	HWND GetWindowHandleByIndex()
 	{
-		/*LPCSTR lpszClassName = "Game Wnidow Class";
-		for (int i = 0; i < win_count; i++)
-		{
-			_win.windows[i] = CreateWindow(lpszClassName, _win.window_name[i], WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0,0, _win.win_width[i], _win.win_height[i], 0, 0, _win.hInstance_[i], 0);
-			_win.hdcs[i] = GetDC(_win.windows[i]);
-		}*/
+		return _win.windows[wnd_param];
+	}
+	void HideWindow()
+	{
+		ShowWindow(GetWindowHandleByIndex(), SW_HIDE);
+	}
+	void ActivateWindow()
+	{
+		ShowWindow(GetWindowHandleByIndex(), SW_SHOW);
+	}
+	void MinimizeWindow()
+	{
+		ShowWindow(GetWindowHandleByIndex(), SW_FORCEMINIMIZE);
+	}
+	void MaximizeWindow()
+	{
+		ShowWindow(GetWindowHandleByIndex(), SW_SHOWMAXIMIZED);
+	}
+	void RestoreWindowState()
+	{
+		ShowWindow(GetWindowHandleByIndex(), SW_SHOWDEFAULT);
+	}
+	//To change window's size use SetWindowPos in simulate_game() loop
+	void change_win_x(int x_)
+	{
+		_win.winx[wnd_param] = x_;
+		SetWindowPos(GetWindowHandleByIndex(), HWND_NOTOPMOST, x_, _win.winy[wnd_param], _win.win_width[wnd_param], _win.win_height[wnd_param], SWP_NOSIZE | SWP_NOZORDER);
+		x = x_;
+	}
+	void change_win_y(int y_)
+	{
+		_win.winy[wnd_param] = y_;
+		SetWindowPos(GetWindowHandleByIndex(), HWND_NOTOPMOST, _win.winx[wnd_param], y_, _win.win_width[wnd_param], _win.win_height[wnd_param], SWP_NOSIZE | SWP_NOZORDER);
+		y = y_;
+	}
+	void change_win_name(LPCSTR name_)
+	{
+		_win.window_name[wnd_param] = name_;
+		//SendMessage(_win.windows[], WM_SETTEXT, 0, name_);
+		SetWindowTextA(_win.windows[wnd_param], name_);
+		name = name_;
+	}
+	void WindowCanBeClosed(bool close_state)
+	{
+		_win.win_can_be_closed[wnd_param] = close_state;
+	}
+	void WindowDestroy()
+	{
+		DestroyWindow(_win.windows[wnd_param]);
 	}
 };
 
@@ -84,9 +150,37 @@ public:
 //template <class T>
 //std::vector<T> name;
 #include "rendering.cpp"
+#include "physics.cpp"
 #include <time.h>
 #include "game.cpp"
-
+//#include <atlstr.h>
+//int FindWindowIndexByName(LPSTR win_name_)
+//{
+//	LPCSTR wnd_name = (LPCSTR)win_name_;
+//	CStringA wnd_name_CString(wnd_name);
+//	int i = 0;
+//	for (i = 0; i < MAX_WINDOW_QUANTITY; i++)
+//	{
+//		CStringA win_class_name_CString(_win.window_name[i]);
+//		if (wnd_name_CString == win_class_name_CString)
+//		{
+//			break;
+//		}
+//	}
+//	return i;
+//}
+int FindWindowIndexByHWND(HWND win_hwnd)
+{
+	int i = 0;
+	for (i = 0; i < win_count; i++)
+	{
+		if (_win.windows[i] == win_hwnd)
+		{
+			return i;
+		}
+	}
+	return i;
+}
 LRESULT CALLBACK Window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	//This is function which sends a message when something important is happened(input, minimize, maximize, size changed
@@ -96,7 +190,16 @@ LRESULT CALLBACK Window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 	case WM_CLOSE:
 	case WM_DESTROY:
 	{
-		running = false;
+		int q = FindWindowIndexByHWND(hwnd);
+		if (_win.win_can_be_closed[q])
+		{
+			DestroyWindow(hwnd);
+		}
+		all_win_active = false;
+		//LPSTR win_name = new CHAR[MAX_PATH]; //this is done because i get an error that win_name is uninitialized
+		//GetWindowText(hwnd, win_name, 100);
+		//int index = FindWindowIndexByName(win_name);
+		//visible_windows[index] = false;
 	}
 	break;
 	case WM_SIZE:
@@ -128,17 +231,17 @@ LRESULT CALLBACK Window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPa
 }
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-	//Creating Window(or Window class)
+	//Creating WINDOW(or WINDOW class)
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW || CS_VREDRAW; //CS_HREDRAW - Horizontal; CS_VREDRAW - Vertical 
-	window_class.lpszClassName = "Game Window Class";
+	window_class.lpszClassName = "Game WINDOW Class";
 	window_class.lpfnWndProc = Window_callback;
 
 	//Register Class
 	RegisterClass(&window_class);
 	for (int i = 0; i < win_count; i++)
 	{
-		_win.windows[i] = CreateWindow(window_class.lpszClassName, _win.window_name[i], WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, _win.win_width[i], _win.win_height[i], 0, 0, hInstance, 0);
+		_win.windows[i] = CreateWindow(window_class.lpszClassName, _win.window_name[i], WS_OVERLAPPEDWINDOW | WS_VISIBLE, _win.winx[i], _win.winy[i], _win.win_width[i], _win.win_height[i], 0, 0, hInstance, 0);
 		_win.hdcs[i] = GetDC(_win.windows[i]);
 	}
 	float delta_time = 0.016666f;
@@ -152,7 +255,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		perfomance_frequency = (float)perfomance.QuadPart;
 	}
 
-	while (running)
+	while (all_win_active)
 	{
 		//Input
 		MSG messages[MAX_WINDOW_QUANTITY];
@@ -166,7 +269,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		}
 		//Simulate
 		simulategame();
-		//Creating Window
+		//Creating WINDOW
 		for (int z = 0; z < win_count; z++)
 		{
 			StretchDIBits(_win.hdcs[z], 0, 0, _win.win_width[z], _win.win_height[z], 0, 0, _win.win_width[z], _win.win_height[z], _win.win_memory[z], &_win.bitmap_info[z], DIB_RGB_COLORS, SRCCOPY);
